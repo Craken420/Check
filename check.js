fec2e636127e4ac4d320d8523d7a7188a8d16fdc
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-01-29 18:52:45 -0600

    Modulado

--- check.js
+++ check.js
@@ -1,110 +1,20 @@
-const iconvlite = require('iconv-lite')
-const fs = require('fs')
-const path = require('path')
+/*** Archivos ***/
+const leerCarpeta = require('./Utilerias/OperadoresArchivos/leerValidarCarpeta')
+const separarEsp = require('./Utilerias/OperadoresArchivos/separarCmpIncorrectoEsp')
 
-<<<<<<< HEAD
-const archivosOriginales    = 'ArchivosOriginales\\'
-const recodificacion        = 'Latin1'
-=======
-const archivosOriginales = 'ArchivosOriginales\\'
-const recodificacion = 'Latin1'
->>>>>>> c6f94e1f0b9f85c1cf7b098d2259e04636dccebf
+/*** Operadores de archivos ***/
+const filtro = require('./Utilerias/OperadoresArchivos/filtrarArchivos')
+const recodificar = require('./Utilerias/Codificacion/contenidoRecodificado')
 
-function crearExpresion (texto) {
-    return new RegExp(`\\[(?!\\w+;)(?!${texto})(?=.*?\\/.*?\\])[^~]*?(?=(\\n|)^\\[)`, `gim`)
-}
+const carpeta = 'ArchivosOriginales\\'
 
-function transformar (archivo, texto) {
-    if( !/\w+/g.test(texto) ) {
-        fs.unlinkSync(archivo)
-        return
-    }
-
-    texto           = texto + '\n['
-    let textoBorrar = texto
-
-    texto           = texto.replace(/^;.*/gm, '')
-    let archivoFrm  = archivo.replace(/.*(\/|\\)|\_MAVI.*|\.esp/gi, '')
-    archivoFrm      = archivoFrm.replace(/\_(?=(frm|vis|tbl|dlg|rep))/gi, '.')
-
-    archivoFrm      = archivoFrm.replace(/(?<=\.)\w+$/gim, archivoFrm => {
-        return archivoFrm.toLowerCase()
+leerCarpeta.obtenerArchivos(carpeta)
+    .then(archivos => {
+        filtro.filtrarExtension(archivos).forEach(archivo => {
+            separarEsp.crearArchivoCmpAddCmpArchivo(
+                archivo,
+                recodificar.extraerContenidoRecodificado(archivo))
+            console.log('Procesando...')
+        })
     })
-
-    let expresion = crearExpresion(archivoFrm)
-
-    if(texto.match(expresion) != null) {
-
-        let txtExtraccion   = texto.match(expresion).join('\n')
-        txtExtraccion       = txtExtraccion + '\n['
-        let nomNuevoArchivo = txtExtraccion.match(/(?<=\[).*?\.(frm|vis|tbl|dlg|rep)/gi)
-        
-        if(nomNuevoArchivo != null) {
-      
-            let set                 = new Set( nomNuevoArchivo.map( JSON.stringify ) )
-            let arrSinDuplicaciones = Array.from( set ).map( JSON.parse )
-            
-            let nombresArchivos = arrSinDuplicaciones.map(x => {
-                x = x.replace(/\.(?=(frm|vis|tbl|dlg|rep))/gi, '_') + '_MAVI.esp'
-                x = x.replace(/(?<=\_)\w+(?=\_)/gi, x => x.toUpperCase())
-                return x
-            })
-
-            for (key2 in arrSinDuplicaciones) {
-                let regExNuevoArchivo   = `\\[${arrSinDuplicaciones[key2]}[^~]*?(?=\\[)`
-                let extraerAccion       = new RegExp(`${regExNuevoArchivo}`, `gi`)
-                let resBool             = txtExtraccion.match(extraerAccion)
-                let txtFinal            = resBool.join('\n')
-
-                txtFinal = txtFinal.replace(/((?=[\ \t])|^\s+|$)+/mg, '')
-                txtFinal = txtFinal.replace(/\[/g, ' \n[')
-                fs.appendFileSync(archivosOriginales + nombresArchivos[key2], '\n' + txtFinal, { flag:'as' })
-            }
-        }
-
-        textoBorrar     = textoBorrar.replace(expresion, '')
-        textoBorrar     = textoBorrar.replace(/\[(?!(\s+|)\w)/gi, '')
-
-        textoComentario = textoBorrar.replace(/^;.*/gm, '')
-        
-        if ( !/\w+/g.test(textoBorrar) || !/\w+/g.test(textoComentario) ) {
-            fs.unlinkSync(archivo)
-            return
-        }
-
-        remplazarTexto(archivo, textoBorrar)
-
-    }
-}
-
-function recodificar(archivo, recodificacion) {
-    return iconvlite.decode(fs.readFileSync(archivo), recodificacion)
-}
-
-function remplazarTexto (archivo, texto) {
-    fs.writeFile(archivo, texto, err => {
-        if (err)  return console.log(err)
-    })
-}
-
-function filtrarExtension (archivos) {
-    return archivos.filter(archivo => /\.esp$/i.test(archivo))
-}
-
-function comprobar (carpeta, archivos) {
-    filtrarExtension(archivos).map(archivo => {
-        return path.join(carpeta, archivo)
-    }).filter(archivo => {
-        // return archivo == 'ArchivosOriginales\\Usuario_FRM_MAVI.esp.esp' 
-        //     || archivo == 'ArchivosOriginales\\ActivarDesafectar.esp'
-        //     || archivo == 'ArchivosOriginales\\UsuarioCfg2_FRM_MAVI.esp'
-        return fs.statSync(archivo).isFile()
-    }).forEach(archivo => {
-        transformar (archivo, recodificar(archivo, recodificacion))
-    })
-}
-
-fs.readdir(archivosOriginales, (error, archivos) => {
-    if (error) throw error
-    comprobar(archivosOriginales, archivos)
-})
+    .catch(e => console.error(e))
